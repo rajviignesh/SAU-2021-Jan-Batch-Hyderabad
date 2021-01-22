fbba5fb4e0baadff33b63ac0bb09f98e25718e3b
rajviignesh <vickydeepak95@gmail.com>
2021-01-22 23:04:51 +0530

    Completed Maven Assignment with Screenshots

---
+++ Maven-Afternoon/webApp/src/main/java/DemoServlet.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DemoServlet extends HttpServlet {
+    private static final long serialVersionUID = -4751096228274971485L;
+    
+    @Override
+    protected void doGet(HttpServletRequest reqest, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1> Hello World from Servlet! <h1>");
+    }
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Servlet " + this.getServletName() + " has started");
+    }
+    @Override
+    public void destroy() {
+        System.out.println("Servlet " + this.getServletName() + " has stopped");
+    }
+}
